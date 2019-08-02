08162ff28585d562889bd24aff21036c46bc870f
drmohotti <drmohotti@gmail.com>
2019-08-02 16:22:25 +0530

    second_drm

--- app/src/main/java/lk/sliit/finestay/MainActivity.java
+++ app/src/main/java/lk/sliit/finestay/MainActivity.java
@@ -2,13 +2,32 @@ package lk.sliit.finestay;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button b1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        b1 = findViewById(R.id.login_redirect);
+
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        b1.setOnClickListener((new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,login_page.class);
+                startActivity(intent);
+            }
+        }));
     }
 }
