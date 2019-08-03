5729f3ad8f2e870ed69894755fbe67aeaad04060
kvsh <>
2019-08-03 08:53:29 +0530

    Added CalculationLayout

---
+++ app/src/main/java/lk/sliit/finestay/CalculationActivity.java
@@ -0,0 +1,17 @@
+package lk.sliit.finestay;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CalculationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.calculation_layout);
+
+    }
+
+
+}
